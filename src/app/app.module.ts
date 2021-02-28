51f5d074f08d5a06181972b68135ce32c26e5fef
Kanvi <kanvisavani123@gmail.com>
2021-02-28 11:50:10 -0500

    added sarch feature?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { SearchResultsComponent } from './search-results/search-results.componen
 import { ShowInfoComponent } from './show-info/show-info.component';
 import { ShowDetailsComponent } from './show-details/show-details.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { WatchlistComponent } from './watchlist/watchlist.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  query = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(): void {
+    console.log(this.query);
   }
 
 }
